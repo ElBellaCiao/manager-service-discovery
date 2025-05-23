fc0b9486ea8a344f824307aa2017ea4d93f37921
ElBellaCiao <aguilarvtrading@gmail.com>
2025-05-23 01:57:38 +0000

    update resources

--- resources/test_put_request.sh
+++ resources/test_put_request.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -X PUT https://k1vptg40ha.execute-api.us-east-1.amazonaws.com/service-discoverability/i-03bfa510b332963dd \
+curl -X PUT https://k1vptg40ha.execute-api.us-east-1.amazonaws.com/service-discoverability/i-0c2b5116e3d789615 \
   -H "Content-Type: application/json" \
   -d @resources/service_discoverability_put_request.json
