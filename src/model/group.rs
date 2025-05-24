9725e208bae35ec9a3e113fed5ee83a2474717d4
ElBellaCiao <aguilarvtrading@gmail.com>
2025-05-24 21:54:26 +0000

    Add additional traits to the Group struct

--- src/model/group.rs
+++ src/model/group.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use strum_macros::{Display, EnumString};
 
-#[derive(EnumString, Display, Serialize, Deserialize, Debug, Eq, PartialEq, Hash)]
+#[derive(EnumString, Display, Serialize, Deserialize, Debug, Eq, PartialEq, Hash, Clone, Copy)]
 #[strum(serialize_all = "lowercase")]
 #[serde(rename_all = "lowercase")]
 pub enum Group {
