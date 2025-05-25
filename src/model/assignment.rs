be53071f8af2d44d157859a013be533a0a0526ec
ElBellaCiao <aguilarvtrading@gmail.com>
2025-05-25 17:45:41 +0000

    Remove SK and PK serde

--- src/model/assignment.rs
+++ src/model/assignment.rs
@@ -6,22 +6,16 @@ use cloud_util::InstanceId;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Assignment {
-    // Name required by ddb
-    #[serde(rename = "PK")]
     pub group: Group,
-
-    // Name required by ddb
-    #[serde(rename = "SK")]
     pub instance_id: InstanceId,
-
-    // format required by ddb
-    #[serde(with = "chrono::serde::ts_seconds")]
-    pub expire_at: DateTime<Utc>,
-
     pub stocks: Vec<String>,
     pub input: Group,
     pub output: Group,
     pub ip: IpAddr,
+
+    // format required by ddb
+    #[serde(with = "chrono::serde::ts_seconds")]
+    pub expire_at: DateTime<Utc>,
 
     // Consider adding a status field.
 }
