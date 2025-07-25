932ac6b2e71cc164967ebd0e93142277b1aa6790
ElBellaCiao <aguilarvtrading@gmail.com>
2025-07-25 02:27:56 +0000

    remove async

--- manager-service-discovery-client/src/lib.rs
+++ manager-service-discovery-client/src/lib.rs
@@ -3,24 +3,31 @@ mod model;
 mod service;
 
 use crate::service::ServiceDiscoveryClient;
+use anyhow::Result;
 use cloud_util::{Ec2MetadataClient, Metadata, get_config};
 use config::Config;
 pub use model::*;
+use tokio::runtime::Runtime;
 
-pub async fn get_assignment() -> anyhow::Result<Assignment> {
-    let config = get_config::<Config>()?;
-    let api_client = cloud_util::RestApi::builder().build().await;
-    let service_discovery_client =
-        ServiceDiscoveryClient::new(api_client, &config.manager_service_discovery_url);
+// Todo: get 'Group' via ssm param store
+pub fn get_assignment() -> Result<Assignment> {
+    Runtime::new()?.block_on(async {
+        // config
+        let config = get_config::<Config>()?;
 
-    let metadata_client = Ec2MetadataClient::builder().build().await?;
-    let group = metadata_client.get_tag_value("App").await?;
-    let ip = metadata_client.get_self_id().await?;
+        // clients
+        let api_client = cloud_util::RestApi::builder().build().await;
+        let service_discovery_client =
+            ServiceDiscoveryClient::new(api_client, &config.manager_service_discovery_url);
+        let metadata_client = Ec2MetadataClient::builder().build().await?;
 
-    let request = GetAssignmentRequest {
-        group,
-        ip: ip.parse()?,
-    };
-
-    service_discovery_client.get(request).await
+        // fetch
+        let group = metadata_client.get_tag_value("Group").await?;
+        let ip = metadata_client.get_self_id().await?;
+        let request = GetAssignmentRequest {
+            group,
+            ip: ip.parse()?,
+        };
+        service_discovery_client.get(request).await
+    })
 }
