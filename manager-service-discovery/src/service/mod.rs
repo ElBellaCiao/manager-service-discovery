952d9bf9caa1903a7e638a0d7d3ed4360950c323
ElBellaCiao <aguilarvtrading@gmail.com>
2025-07-25 03:19:36 +0000

    return list of ips

--- manager-service-discovery/src/service/mod.rs
+++ manager-service-discovery/src/service/mod.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use anyhow::{Result, bail};
 use chrono::Utc;
 use lambda_http::Request;
@@ -25,18 +26,33 @@ impl ServiceDiscovery {
 
     pub async fn get_assignment(&self, req: Request) -> Result<Value> {
         let get_request = serde_json::from_slice::<GetAssignmentRequest>(req.body().as_ref())?;
-        let instance_assignment = self
+        let assignment = self
             .table_client
             .get_entry(&get_request.group, &get_request.ip.to_string())
             .await?;
-        if Utc::now() > instance_assignment.expire_at {
+
+        if Utc::now() > assignment.expire_at {
             bail!(
                 "Assignment has expired: current time = {}, expires at = {}",
                 Utc::now(),
-                instance_assignment.expire_at
+                assignment.expire_at
             );
         }
 
-        Ok(json!(instance_assignment))
+        let mut instrument_to_ips = HashMap::new();
+
+        for output_group in assignment.output_groups {
+            let group_assignments = self.table_client.get_entries_by_pk(&output_group).await?;
+            for output_assignment in group_assignments {
+                for instrument_id in output_assignment.instrument_ids {
+                    instrument_to_ips
+                        .entry(instrument_id.to_string())
+                        .or_insert_with(Vec::new)
+                        .push(output_assignment.ip);
+                }
+            }
+        }
+
+        Ok(json!(instrument_to_ips))
     }
 }
